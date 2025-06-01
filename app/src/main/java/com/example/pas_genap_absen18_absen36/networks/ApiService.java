c686bd1f0b7192e71a3a9f5101d01db9b20681a8
ddimadiii <ddimadiii.com>
2025-06-01 20:05:47 +0700

    Merge remote-tracking branch 'origin/develop' into develop

--- app/src/main/java/com/example/pas_genap_absen18_absen36/networks/ApiService.java
+++ app/src/main/java/com/example/pas_genap_absen18_absen36/networks/ApiService.java
@@ -1,23 +1,17 @@
 package com.example.pas_genap_absen18_absen36.networks;
-import com.example.pas_genap_absen18_absen36.models.PlayerResponse;
+
+import com.example.pas_genap_absen18_absen36.models.Team;
 import com.example.pas_genap_absen18_absen36.models.TeamResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-public interface ApiService {
-
-
-
+public interface APIService {
 
     @GET("search_all_teams.php")
     Call<TeamResponse> getSpainLeagueTeams(
             @Query("s") String sport,
             @Query("c") String country
     );
-    @GET("searchplayers.php")
-    Call<PlayerResponse> searchPlayers(@Query("p") String playerName);
-
-
 }
